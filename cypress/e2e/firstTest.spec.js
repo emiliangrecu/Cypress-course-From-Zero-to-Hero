51a3bce4e8ec7b032cd591c39981bb1fabfd1ebd
emiliangrecu <emilian.grecu25@yahoo.com>
2023-04-21 13:54:04 +0300

    implement Page Object Design Pattern

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -281,11 +281,11 @@ describe('Our first suite', () => {
         // })
 
         //2
-        // const stub = cy.stub();
-        // cy.on('window:confirm', stub);
-        // cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
-        //     expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?');
-        // })
+        const stub = cy.stub();
+        cy.on('window:confirm', stub);
+        cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?');
+        })
 
         //3
         cy.get('tbody tr').first().find('.nb-trash').click();
